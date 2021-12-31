3c4d2724243333eb53db25856088f8704f73cf4a
MeSagor <dear.shorifulislam@gmail.com>
2021-12-31 23:41:07 +0600

    bisection and falsePosition method added

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Bisection doBisection = new Bisection(-7, 7, 0.00001);
+        falsePosition falseP = new falsePosition(-7, 7, 0.00001);
+    }
+}
