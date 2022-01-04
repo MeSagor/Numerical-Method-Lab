f2354e254d06dbcfbcc2d45a05b91fda2bd0a53c
MeSagor <dear.shorifulislam@gmail.com>
2022-01-04 23:28:04 +0600

    add newton raphson and secant method

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         Bisection doBisection = new Bisection(-7, 7, 0.00001);
+        System.out.println();
         falsePosition falseP = new falsePosition(-7, 7, 0.00001);
+        System.out.println();
+        newtonRaphson newton = new newtonRaphson(-20.0, 0.00001);
+        System.out.println();
+        Secant sec = new Secant(-2, 2, 0.00001);
     }
 }
